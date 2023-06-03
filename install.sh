afe13c88f36b9f3f4abbb7abffb2e09ad2b838a4
Vlad Leontescu <lvlad27@gmail.com>
2023-06-03 22:29:28 +0300

    feat: add i3 config

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+mkdir -p "$HOME/.config/X11"
 
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 ln -sf "$HOME/dotfiles/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
