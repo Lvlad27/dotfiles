cd446511455752d7885a739087085f17a414a6a9
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2024-01-08 18:27:56 +0200

    feat: add autorandr config for monitor layout

--- X11/laptop.sh
+++ X11/laptop.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
-# laptop.sh - Script to set up the laptop screen as the only active display
-
-# Function to disable a monitor if it is connected
-disable_if_connected() {
-    if xrandr | grep -q "^$1 connected"; then
-        xrandr --output $1 --off
-    fi
-}
-
-# Disable all external monitors
-disable_if_connected HDMI-1
-disable_if_connected DP-1
-disable_if_connected DP-2
-disable_if_connected DVI-I-5-4
-disable_if_connected DVI-I-4-3
-disable_if_connected DVI-I-3-2
-disable_if_connected DVI-I-2-1
-
 # Enable and set the primary monitor to the laptop screen
-xrandr --output eDP-11 --primary --auto
+xrandr --output eDP-1 --primary --auto
