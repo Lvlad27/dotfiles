6b43c8a83a5e748252b2cd84708594a907de803a
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2024-04-06 13:03:18 +0300

    feat: update config for work

--- X11/monitor.sh
+++ X11/monitor.sh
@@ -5,4 +5,4 @@
 xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal \
        --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
        --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
-       --output DVI-I-2-1 --mode 1920x1080 --pos 5760x0 --rotate normal
+       --output DVI-I-3-2 --mode 1920x1080 --pos 5760x0 --rotate normal
--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -2,3 +2,6 @@
 compress() {
     tar cvzf $1.tar.gz $1
 }
+killport() {
+  kill -9 $(lsof -t -i :$1)
+}
