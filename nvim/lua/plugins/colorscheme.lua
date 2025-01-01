8fd4010cb070b08d907ee8f0d588ba480c7d551b
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-01 21:20:22 +0200

    feat(neovim-theme): remove italics and add preffered colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,26 @@
 return {
-  -- Configure LazyVim to load kanagawa
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rosebones",
-    },
-  },
-
   {
     "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
     dependencies = "rktjmp/lush.nvim",
     lazy = false,
     priority = 1000,
-    -- you can set set configuration options here
-    -- config = function()
-    --     vim.g.zenbones_darken_comments = 45
-    --     vim.cmd.colorscheme('zenbones')
-    -- end
+    config = function()
+      -- Create autocmd group and command before setting colorscheme
+      local grpid = vim.api.nvim_create_augroup("custom_highlights", {})
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = grpid,
+        pattern = "*bones",
+        command = "hi Comment gui=NONE |"
+          .. "hi Constant gui=NONE |"
+          .. "hi String gui=NONE |"
+          .. "hi Boolean gui=NONE",
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "zenbones",
+    },
   },
 }
