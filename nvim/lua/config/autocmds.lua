cd9ff56eaa962baa3ef3f5eb25592a1accca3d79
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-02 23:14:08 +0200

    fix: remove merge conflict annotations

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,7 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 --
 -- Add any additional autocmds here
-<<<<<<< HEAD
 --
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "text" },
@@ -17,9 +16,3 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.wo.conceallevel = 0
   end,
 })
-=======
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
->>>>>>> 803bc181d7c0d6d5eeba9274d9be49b287294d99
