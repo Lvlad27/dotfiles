50ea185768103dccb6a75a0cdb5701d00ab8b966
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-02 23:12:45 +0200

    feat(neovim): update config
    - remove json and md concelment
    - enable not showing md lin line rule
    - add solarized osaka theme
    - remove inlay hints

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
+<<<<<<< HEAD
 --
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "text" },
@@ -8,3 +10,16 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
+=======
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+>>>>>>> 803bc181d7c0d6d5eeba9274d9be49b287294d99
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      vim.g.zenbones = {
+        transparent_background = true,
+      }
       -- Create autocmd group and command before setting colorscheme
       local grpid = vim.api.nvim_create_augroup("custom_highlights", {})
       vim.api.nvim_create_autocmd("ColorScheme", {
@@ -17,6 +20,14 @@ return {
       })
     end,
   },
+
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
   {
     "LazyVim/LazyVim",
     opts = {
