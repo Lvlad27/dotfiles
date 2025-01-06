41ebf3863535934eef32cf2c10bb39d272db5a44
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-07 01:27:52 +0200

    feat(neovim): enable visibility of hidden files

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,25 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true, -- This makes hidden files visible by default
-        hide_dotfiles = false,
-        hide_gitignored = false,
+  opts = function() -- Convert opts to a function that returns the configuration
+    vim.cmd([[hi NeoTreeNormal guibg=NONE ctermbg=NONE]]) -- Execute the highlight command
+
+    return { -- Return the configuration table
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer
+        },
       },
-    },
-  },
+      window = {
+        mappings = {
+          ["w"] = "open", -- This adds 'w' as a key to open files
+          ["<cr>"] = "", -- This disables the enter key mapping
+        },
+      },
+    }
+  end,
 }
