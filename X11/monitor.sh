48f1381d440523392a2a57b18ee7628e345d02d4
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-13 12:48:54 +0200

    fix: primary monitor colors are not full rgb

--- X11/monitor.sh
+++ X11/monitor.sh
@@ -3,6 +3,10 @@
 
 #!/bin/sh
 xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal \
-       --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-       --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
-       --output DVI-I-3-2 --mode 1920x1080 --pos 5760x0 --rotate normal
+  --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+  --output DVI-I-3-2 --mode 1920x1080 --pos 5760x0 --rotate normal
+
+# Apply color correction for DP-1
+xrandr --output DP-1 --set "Broadcast RGB" "Full"
+
