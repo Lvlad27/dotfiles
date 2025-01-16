845ac84df7f1ec55f94c812a23a7c4bd8ad9c4a3
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-16 14:10:45 +0200

    fix(x11): central monitor now displays full intense colors

--- X11/monitor.sh
+++ X11/monitor.sh
@@ -8,5 +8,6 @@ xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal \
   --output DVI-I-3-2 --mode 1920x1080 --pos 5760x0 --rotate normal
 
 # Apply color correction for DP-1
-xrandr --output DP-1 --set "Broadcast RGB" "Full"
-
+for output in $(xrandr | grep " connected" | cut -d " " -f1); do
+  xrandr --output "$output" --set "Broadcast RGB" "Full" 2>/dev/null
+done
