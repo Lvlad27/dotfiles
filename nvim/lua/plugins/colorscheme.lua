1958aea8dff208ab82c729509a13a7969fbcb803
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-25 18:06:37 +0200

    feat: update color theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -10,11 +10,23 @@ return {
       },
     },
   },
-
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      variant = "dark", -- or "light"
+      background = "hard", -- "soft", "medium", "hard"
+    },
+    config = function(_, opts)
+      vim.o.background = opts.variant
+      vim.g.everforest_background = opts.background
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "everforest",
     },
   },
 }
