5d566655b215c8f27e5fa8a9e4dba9d48e706625
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-01-28 15:33:50 +0200

    feat: add light/dark theme cycle

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -20,9 +20,41 @@ return {
     end,
   },
   {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "everforest",
-    },
+    opts = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimStarted",
+        callback = function()
+          -- Ensure theme is loaded
+          require("lazy").load({ plugins = { "github-theme" } })
+
+          local hour = tonumber(os.date("%H"))
+          local light_start = 7
+          local light_end = 18
+
+          if hour >= light_start and hour < light_end then
+            vim.opt.background = "light"
+            vim.cmd("colorscheme github_light_colorblind")
+          else
+            vim.opt.background = "dark"
+            vim.cmd("colorscheme everforest")
+          end
+        end,
+      })
+
+      -- Return empty opts to prevent default colorscheme
+      return {}
+    end,
   },
 }
