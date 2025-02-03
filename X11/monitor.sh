e50e017e9a26b8443fd97ba107a450afb708093c
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2025-02-03 09:28:38 +0200

    fix: add proper white background color

--- X11/monitor.sh
+++ X11/monitor.sh
@@ -14,7 +14,7 @@
 #
 xrandr --output eDP-1 --mode 1920x1080 --pos 1920x1080 --rotate normal \
   --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-  --output DP-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+  --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
   --output DVI-I-2-1 --mode 1920x1080 --pos 3840x0 --rotate normal
 
 # Apply color correction to all connected monitors
