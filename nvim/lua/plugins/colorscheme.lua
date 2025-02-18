157fb86bc86df107fddd4098a2a4608b84387c35
Vlad Leontescu <lvlad27@gmail.com>
2025-02-18 09:54:39 +0200

    feat(neovim): update default color scheme and add custom prompts for copilot

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -15,7 +15,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_light_colorblind",
+      colorscheme = "rose-pine-main",
     },
   },
 }
--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -8,6 +8,14 @@ return {
       window = {
         width = 0.4,
       },
+      prompts = {
+        REWRITE = {
+          prompt = "Rewrite this using words more suitable for non-native english speakers. If possible, choose short words.",
+        },
+        SUMMARY = {
+          prompt = "Create a single paragraph status report for all the completed todos in this file. Use simple and short words if possible. Structure the status report in bullet points by task, following with the descriptions.\nThis should be the format:\n\nHello! For today:\n\n- Task code: todos gathered in a single paragraph\n- Task code: todos gathered in a single paragraph\n",
+        },
+      },
     },
     build = function()
       vim.notify("Please install 'pip install python-dotenv'")
